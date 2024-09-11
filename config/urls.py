76a7953dfc79a94fce767dabbc207e13c89f8cb3
TinyPuff <ghost1of1king@gmail.com>
2024-09-12 01:22:56 +0330

    changed admin panel url

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("chapelofghouls/", admin.site.urls),
     path("api/v1/users/", include("users.urls")),
 ]
