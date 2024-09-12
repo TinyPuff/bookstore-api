749b041e0318f65f3587d9b178eb6a29e3e21e0e
TinyPuff <ghost1of1king@gmail.com>
2024-09-12 16:42:43 +0330

    minor changes

--- books/views.py
+++ books/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions
 from .models import Book, Category, Review
 from .serializers import BookSerializer, CategorySerializer, ReviewSerializer
 
