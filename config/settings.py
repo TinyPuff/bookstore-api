93baa3b25eab0549eec1ea7b544534eaf4889df7
TinyPuff <ghost1of1king@gmail.com>
2024-09-12 12:26:46 +0330

    installed dj-rest-auth and django-allauth

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # Third-party
     "rest_framework",
     "corsheaders",
     "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
     # Local
     "users.apps.UsersConfig",
 ]
@@ -76,6 +82,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
@@ -139,6 +146,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Authentication and django-allauth config
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+# E-mail config
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+SITE_ID = 1 
 
 # Django Rest Framework config
 
