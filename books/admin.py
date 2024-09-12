7d5d174e752613f2ba32fb883af78f6c43a02648
TinyPuff <ghost1of1king@gmail.com>
2024-09-12 16:29:54 +0330

    all book models are now shown in the admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
-from .models import Book, Category
+from .models import Book, Category, Review
 
 # Register your models here.
 
 
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+
 class BookAdmin(admin.ModelAdmin):
     model = Book
+    inlines = [
+        ReviewInline,
+    ]
     list_display = ("title", "author", "price", "stock")
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    fields = ("title",)
+
+
 admin.site.register(Book, BookAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
