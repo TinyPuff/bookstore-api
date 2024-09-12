b80a594f634cce1e2b77e11f99f0cbe8eba8b54f
TinyPuff <ghost1of1king@gmail.com>
2024-09-13 00:44:23 +0330

    added UsersCartView that lists all the cart objects belonging to a user

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import SimpleRouter
-from .views import CartViewSet
+from .views import CartViewSet, UserCartView
+from django.urls import path
 
 
 router = SimpleRouter()
 router.register("cart", CartViewSet, basename="cart")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("cart/users/<int:user_id>/", UserCartView.as_view(), name="user_cart"),
+] + router.urls
--- orders/views.py
+++ orders/views.py
@@ -3,8 +3,11 @@ from rest_framework.response import Response
 from .models import Cart
 from .serializers import CartSerializer
 from allauth.account.models import EmailAddress
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+
+User = get_user_model()
 
 
 class CartViewSet(viewsets.ModelViewSet):
@@ -12,7 +15,7 @@ class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
 
-    def create(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):  # Prevents the creation of duplicates
         email = EmailAddress.objects.get(email=request.data.get("user"))
         product = int(request.data.get("product"))
         cart = Cart.objects.filter(user=email, product=product)
@@ -24,3 +27,15 @@ class CartViewSet(viewsets.ModelViewSet):
             )
 
         return super().create(request, *args, **kwargs)
+
+
+class UserCartView(generics.ListCreateAPIView):
+    """Lists all cart items belonging to a specific user based on the passed user_id in the url"""
+
+    permission_classes = (permissions.IsAdminUser,)
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        user = User.objects.get(id=int(user_id))
+        return Cart.objects.filter(user=EmailAddress.objects.get(email=user.email))
