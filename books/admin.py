644cb5eb5a6c31a20ecc3058c3d08c13d11500a1
TinyPuff <ghost1of1king@gmail.com>
2024-09-12 14:08:48 +0330

    Book and Category models are now in the admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, Category
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+    list_display = ("title", "author", "price", "stock")
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Category)
